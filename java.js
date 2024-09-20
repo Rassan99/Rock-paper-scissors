5d1982448150d3d863b9cda8532d2bcaf4f074a0
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2024-09-20 14:12:48 +0300

    Revert "Feat: Added the computer function"
    This reverts commit 9873c6fa04959ac6231246e5887319363a66530d.

--- java.js
+++ java.js
@@ -28,8 +28,6 @@
     
 // }
 // button.addEventListener("click", myFunction);
-radio=document.getElementsByName('drone');
-
 function computer() {
     return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
   }
@@ -42,7 +40,7 @@ function human(){
 function onClick() {
    label.textContent = 'Error: one or both inputs are empty.';
   }
-let label = document.querySelector('p');
-let button = document.querySelectorAll('.button');
-radio.addEventListener('click', onClick);
+var label = document.querySelector('p');
+var button = document.querySelector('button');
+button.addEventListener('click', onClick);
 
